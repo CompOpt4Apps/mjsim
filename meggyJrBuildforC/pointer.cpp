24ac8d05ceb1f64e8b352bcb2e9296e833d98316
mstrout <mstrout@6a19b1a8-a6fa-c999-6c1d-2fce1911f672>
2011-01-23 19:47:23 +0000

    Got malloc to compile in pointer.cpp with Makefile.mac, make pointer.s.

--- meggyJrBuildforC/pointer.cpp
+++ meggyJrBuildforC/pointer.cpp
@@ -1,12 +1,12 @@
 #include "MeggyJr.h"
-#include <avr/stlib.h>
+#include <stdlib.h>
 
 int main (void)
 {
 	char x = 10;
 	char *p = &x;
 	char y = *p;
-    p = malloc(10);
+    p = (char*)malloc(10);
     p[3] = 5;
 	return 0;
 }
--- meggyJrBuildforC/pointer.s
+++ meggyJrBuildforC/pointer.s
@@ -7,3 +7,20 @@ __tmp_reg__ = 0
 __zero_reg__ = 1
 	.global __do_copy_data
 	.global __do_clear_bss
+	.text
+.global	main
+	.type	main, @function
+main:
+/* prologue: function */
+/* frame size = 0 */
+	ldi r24,lo8(10)
+	ldi r25,hi8(10)
+	call malloc
+	movw r30,r24
+	ldi r24,lo8(5)
+	std Z+3,r24
+	ldi r24,lo8(0)
+	ldi r25,hi8(0)
+/* epilogue start */
+	ret
+	.size	main, .-main
