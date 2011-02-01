1f00ad512472501a969c32da6e727da40de493a5
moorer <moorer@6a19b1a8-a6fa-c999-6c1d-2fce1911f672>
2011-02-01 09:56:18 +0000

    Have an example of adding data to the TableView from within Java.

--- src/ui/components/BaseWindow.java
+++ src/ui/components/BaseWindow.java
@@ -4,6 +4,8 @@ import java.net.URL;
 
 import org.apache.log4j.Logger;
 import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.HashMap;
+import org.apache.pivot.collections.List;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
 import org.apache.pivot.wtk.TableView;
@@ -15,11 +17,17 @@ public class BaseWindow extends Window implements Bindable {
 	
 	private TableView stackPane;
 	private TableView heapPane;
+	@SuppressWarnings("unchecked")
 	@Override
 	public void initialize(Map<String, Object> arg0, URL arg1, Resources arg2) {
 		logger.info("Initializing window.");
 		stackPane = (TableView) arg0.get("stackTable");
 		heapPane = (TableView) arg0.get("heapTable");
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("address", "0x3");
+		map.put("value","23");
+		((List<HashMap<String,String>>)(stackPane.getTableData())).add(map);
+
 		}
 
 }
