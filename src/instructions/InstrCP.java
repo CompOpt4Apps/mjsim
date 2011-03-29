0a14665fffa7cc3179cde0f2fe9f2b1214fcac10
mstrout <mstrout@6a19b1a8-a6fa-c999-6c1d-2fce1911f672>
2011-03-29 04:28:40 +0000

    Ok, NOW InstrCP works and all the MeggySim and MeggyRef regression tests pass.

--- src/instructions/InstrCP.java
+++ src/instructions/InstrCP.java
@@ -12,6 +12,7 @@ public class InstrCP extends Instr {
 	private final int rr;
 	private final static int bitMask = 0xFF;
 	private final static int msbMask = 0x80;
+	private final static int bit7Mask = 0x40;
 	public InstrCP(MachineState machine, int rd, int rr) throws MalformedInstruction {
 		super(machine);
 		if(rd < 0 || rd > 31)
@@ -42,7 +43,7 @@ public class InstrCP extends Instr {
 		int src = this.machine.getRegister(rr);
         logger.debug("dst = "+dst+", src = "+src);
 		int nMsb = (dst & msbMask) & (src & msbMask);//used for checking to see if the msb has changed.
-		int result = (dst - src) & bitMask;
+		int result = (dst - src); // & bitMask;
         logger.debug("result = "+result);
 		this.event.setPC(this.machine.getPC()+1);//update pc value.
 		//check for the C bit.
@@ -60,30 +61,42 @@ public class InstrCP extends Instr {
 		if(result == 0)
 		{
 			newSREG.setZ(true);
+			logger.trace("Setting Z to true");
 		}
 		else
 		{
 			newSREG.setZ(false);
+			logger.trace("Setting Z to false");
 		}
 		
 		//check for the N bit
 		if((result & msbMask) == msbMask)
 		{
 			newSREG.setN(true);
+			logger.trace("Setting N to true");
 		}
 		else
 		{
 			newSREG.setN(false);
+			logger.trace("Setting N to false");
 		}
 		
 		//check for the V bit.
-		if((result & msbMask) != nMsb)
+		// Rd7 and !Rr7 and !R7 + !Rd7 and Rr7 and R7
+        // Set if two’s complement overflow resulted 
+        // from the operation; cleared otherwise.
+        if ( ((bit7Mask & dst)>0 && (bit7Mask & src)==0 && (bit7Mask&result)==0) 
+		  || ((bit7Mask & dst)==0 && (bit7Mask & src)>0 && (bit7Mask&result)>0) 
+		   )
+		//if((result & msbMask) != nMsb)
 		{
-			newSREG.setN(true);
+			newSREG.setV(true);
+			logger.trace("Setting V to true");
 		}
 		else
 		{
-			newSREG.setN(false);
+			newSREG.setV(false);
+			logger.trace("Setting V to false");
 		}
 		
 		//determine S
